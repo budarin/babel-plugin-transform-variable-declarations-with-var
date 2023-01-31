9fba1a9d823601cda2ea26b315d4597b1489a157
Vadim Budarin <budarin-vv@yandex.ru>
2023-01-31 22:36:57 +0300

    test: refactor

--- test/fixtures/transform-variable-declarations-with-var/code.js
+++ test/fixtures/transform-variable-declarations-with-var/code.js
@@ -1,6 +1,8 @@
 let b = 'a';
 const a = 1;
 
+const array = [1, 2, 3];
+
 for (let i = 0; i < array.length; i++) {
     b = b + String(array[i]);
 }
--- test/fixtures/transform-variable-declarations-with-var/output.js
+++ test/fixtures/transform-variable-declarations-with-var/output.js
@@ -1,5 +1,6 @@
 var b = 'a';
 var a = 1;
+var array = [1, 2, 3];
 for (var i = 0; i < array.length; i++) {
     b = b + String(array[i]);
 }
